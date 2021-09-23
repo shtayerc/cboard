fd678c2432d2e8ad32cd9b7f99e7cdadbcb42577
shtayerc <david.murko@mailbox.org>
2021-09-23 12:56:04 +0200

    explorer: lichess - show opening if not null

--- explorers/lichess.sh
+++ explorers/lichess.sh
@@ -6,8 +6,11 @@ do
     case "$command" in
         "fen")
             fen="$(echo "$input" | cut -d' ' -f2-)"
-            response="$(curl -sX 'GET' --data-urlencode "fen=$fen" "$url" | jq -rcM '.moves[] | "\(.san) (\(.white+.black+.draws)) [\((.white/(.white+.draws+.black)*100) | round)% | \((.draws/(.white+.draws+.black)*100) | round)% | \((.black/(.white+.draws+.black))*100 | round)%]"')"
-            echo "$response"
+            response="$(curl -sX 'GET' --data-urlencode "fen=$fen" "$url")"
+            opening="$(echo "$response" | jq -rcM '.opening | "\(.eco) \(.name)"')\n"
+            [ "$opening" = "null null\n" ] && opening=""
+            moves="$(echo "$response" | jq -rcM '.moves[] | "\(.san) (\(.white+.black+.draws)) [\((.white/(.white+.draws+.black)*100) | round)% | \((.draws/(.white+.draws+.black)*100) | round)% | \((.black/(.white+.draws+.black))*100 | round)%]"')"
+            echo "${opening}${moves}"
             ;;
 
         *)
