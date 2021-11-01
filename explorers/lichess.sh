311dc87ac9dbcc07e66ab54de1c2308f9af80de0
shtayerc <david.murko@mailbox.org>
2021-11-01 09:17:25 +0100

    explorer: lichess - curl fix

--- explorers/lichess.sh
+++ explorers/lichess.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-url="https://explorer.lichess.ovh/master"
+url="https://explorer.lichess.ovh/masters"
 while read input
 do
     command="$(echo "$input" | awk '{print $1}')"
     case "$command" in
         "fen")
             fen="$(echo "$input" | cut -d' ' -f2-)"
-            response="$(curl -sX 'GET' --data-urlencode "fen=$fen" "$url")"
+            response="$(curl -sG --data-urlencode "fen=$fen" "$url")"
             opening="$(echo "$response" | jq -rcM '.opening | "\(.eco) \(.name)"')\n"
             [ "$opening" = "null null\n" ] && opening=""
             moves="$(echo "$response" | jq -rcM '.moves[] | "\(.san) (\(.white+.black+.draws)) [\((.white/(.white+.draws+.black)*100) | round)% | \((.draws/(.white+.draws+.black)*100) | round)% | \((.black/(.white+.draws+.black))*100 | round)%]"')"
