eab74900780d50e9b314c163e69c5df8ffb2ad03
shtayerc <david.murko@mailbox.org>
2024-04-18 18:31:39 +0200

    Update chess_utils

--- src/chess_utils.h
+++ src/chess_utils.h
@@ -1,5 +1,5 @@
 /*
-chess_utils v0.7.11
+chess_utils v0.7.12
 
 Copyright (c) 2024 David Murko
 
@@ -53,71 +53,17 @@ extern "C" {
 //
 
 typedef enum {
-    a8 = 0,
-    b8 = 1,
-    c8 = 2,
-    d8 = 3,
-    e8 = 4,
-    f8 = 5,
-    g8 = 6,
-    h8 = 7,
-    a7 = 16,
-    b7 = 17,
-    c7 = 18,
-    d7 = 19,
-    e7 = 20,
-    f7 = 21,
-    g7 = 22,
-    h7 = 23,
-    a6 = 32,
-    b6 = 33,
-    c6 = 34,
-    d6 = 35,
-    e6 = 36,
-    f6 = 37,
-    g6 = 38,
-    h6 = 39,
-    a5 = 48,
-    b5 = 49,
-    c5 = 50,
-    d5 = 51,
-    e5 = 52,
-    f5 = 53,
-    g5 = 54,
-    h5 = 55,
-    a4 = 64,
-    b4 = 65,
-    c4 = 66,
-    d4 = 67,
-    e4 = 68,
-    f4 = 69,
-    g4 = 70,
-    h4 = 71,
-    a3 = 80,
-    b3 = 81,
-    c3 = 82,
-    d3 = 83,
-    e3 = 84,
-    f3 = 85,
-    g3 = 86,
-    h3 = 87,
-    a2 = 96,
-    b2 = 97,
-    c2 = 98,
-    d2 = 99,
-    e2 = 100,
-    f2 = 101,
-    g2 = 102,
-    h2 = 103,
-    a1 = 112,
-    b1 = 113,
-    c1 = 114,
-    d1 = 115,
-    e1 = 116,
-    f1 = 117,
-    g1 = 118,
-    h1 = 119,
+    // clang-format off
+    a8=   0, b8=   1, c8=   2, d8=   3, e8=   4, f8=   5, g8=   6, h8=   7,
+    a7=  16, b7=  17, c7=  18, d7=  19, e7=  20, f7=  21, g7=  22, h7=  23,
+    a6=  32, b6=  33, c6=  34, d6=  35, e6=  36, f6=  37, g6=  38, h6=  39,
+    a5=  48, b5=  49, c5=  50, d5=  51, e5=  52, f5=  53, g5=  54, h5=  55,
+    a4=  64, b4=  65, c4=  66, d4=  67, e4=  68, f4=  69, g4=  70, h4=  71,
+    a3=  80, b3=  81, c3=  82, d3=  83, e3=  84, f3=  85, g3=  86, h3=  87,
+    a2=  96, b2=  97, c2=  98, d2=  99, e2= 100, f2= 101, g2= 102, h2= 103,
+    a1= 112, b1= 113, c1= 114, d1= 115, e1= 116, f1= 117, g1= 118, h1= 119,
     none = -1
+    // clang-format on
 } Square;
 
 typedef enum { Rank_8, Rank_7, Rank_6, Rank_5, Rank_4, Rank_3, Rank_2, Rank_1, Rank_none = -1 } Rank;
@@ -478,6 +424,9 @@ Variation* variation_clone(Variation* v, Variation* prev);
 //returns move index of given board found in given variation or sub variations.
 //found variation from which index was returned
 int variation_board_find(Variation* v, Board* b, Variation** found);
+
+//reset move_current counter of all recursive variations to 1
+void variation_move_current_reset(Variation *v);
 
 //
 // VARIATION SEQUENCE FUNCTIONS
@@ -2248,6 +2197,17 @@ variation_board_find(Variation* v, Board* b, Variation** found) {
         }
     }
     return index;
+}
+
+void
+variation_move_current_reset(Variation *v) {
+    v->move_current = 1;
+    int i, j;
+    for (i = 0; i < v->move_count; i++) {
+        for (j = 0; j < v->move_list[i].variation_count; j++) {
+            variation_move_current_reset(v->move_list[i].variation_list[j]);
+        }
+    }
 }
 
 void
